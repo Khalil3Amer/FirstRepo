db5bcb6a1e7da32d8b4ce7c7cb382806b7c13c46
Khalil3Amer <KAbdallah@asaltech.com>
2022-04-21 17:06:42 +0300

    models to communcate with database throw

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -1,3 +1,29 @@
-# from django.db import models
+from django.db import models
+from django.utils.timezone import now
 
-# Create your models here.
+
+class Specs(models.Model):
+    color = models.CharField(max_length=10)
+    fuel_type = models.CharField(max_length=10)
+    max_speed = models.IntegerField()
+
+    def __str__(self):
+        return (
+            f"(color: {self.color},fuel_type: {self.fuel_type}, "
+            + f"max_speed: {self.max_speed})"
+        )
+
+
+class Car(models.Model):
+    model = models.CharField(max_length=30)
+    price = models.IntegerField()
+    previous_owner = models.CharField(null=True, max_length=30)
+    date_imported = models.DateTimeField(default=now)
+    specs = models.ForeignKey(Specs, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return (
+            f"model: {self.model}, price: {self.price}, "
+            + f"previous_owner: {self.previous_owner}, date_imported: "
+            + f"{self.date_imported},\nspecs= {self.specs}"
+        )
